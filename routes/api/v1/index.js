047a7c52503baa304038b68431d9e94674fc62b9
Ali Hamood <ali.hamood@gov.bc.ca>
2025-05-30 18:29:40 -0700

    Adds /auth rout.

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import authRouter from './auth-router.js';
 import assetsRouter from './assets-router.js';
 import fileExtensionsRouter from './file-extensions-router.js';
 
 const v1Router = express.Router();
 
+v1Router.use('/auth', authRouter);
 v1Router.use('/assets', assetsRouter);
 v1Router.use('/file-extensions', fileExtensionsRouter);
 
